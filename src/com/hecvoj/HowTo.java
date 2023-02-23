d97f1932a85f138b6098961e390ef4dc2dc98f58
vheczkoslav <vojtech.heczko@gmail.com>
2023-02-23 18:46:12 +0100

    HowTo improved

--- src/com/hecvoj/HowTo.java
+++ src/com/hecvoj/HowTo.java
@@ -5,34 +5,43 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.WindowStateListener;
 
 import static com.sun.java.accessibility.util.AWTEventMonitor.addWindowListener;
 
 public class HowTo{
     JFrame HowTo = new JFrame("How to use this program");
-    HowTo(){
-        HowTo.setSize(1200,800);
-        HowTo.setLocation(50,50);
+    HowTo() {
+        HowTo.setSize(1200, 800);
+        HowTo.setLocation(50, 50);
+        HowTo.setLayout(null);
         HowTo.setVisible(true);
+        HowTo.setResizable(false);
         HowTo.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
 
         drawInfo();
 
-        HowTo.addWindowListener(new WindowAdapter()
-        {
+        HowTo.addWindowListener(new WindowAdapter() {
             @Override
-            public void windowClosing(WindowEvent e)
-            {
+            public void windowClosing(WindowEvent e) {
                 Interface i = new Interface();
                 e.getWindow().dispose();
+            }
+        });
+
+        HowTo.addWindowStateListener(e -> {
+            if(e.getNewState() == Frame.ICONIFIED){
+                System.out.println("Frame has been minimalised");
+                HowTo.setState(Frame.NORMAL);
             }
         });
     }
 
     private void drawInfo() {
-        JLabel mainInfo = new JLabel("This window is WIP. Please close to move to the main program :)");
-        mainInfo.setFont(new Font("Calibri", Font.BOLD, 30));
-        //mainInfo.setBounds(0,0,90,60);
+        JLabel mainInfo = new JLabel("This program is for visual interpretation of planet latitudes based on axial tilt");
+        mainInfo.setFont(new Font("Calibri", Font.BOLD, 20));
+        mainInfo.setBounds(220,0,700,40);
+        mainInfo.setForeground(Color.red);
         mainInfo.setVisible(true);
         HowTo.add(mainInfo);
     }
